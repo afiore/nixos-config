86c71f8c24b4745131685bcf64538f293373a3cb
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2018-09-17 18:03:06 +0100

    symlink to /etc/nixos

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,236 @@
-../../../../etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan..
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot = {
+    enable = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams=["acpi_rev_override=1"];
+
+  # consider enabling to get more recent stuff
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.blacklistedKernelModules = ["nouveau"];
+
+  hardware.bumblebee = {
+    enable = true;
+    driver = "nvidia";
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    firefox = {
+     enableGoogleTalkPlugin = true;
+    };
+    packageOverrides = pkgs: rec {
+      jre = pkgs.oraclejre8;
+      neovim = (import ./vim.nix);
+    };
+  };
+
+  networking.networkmanager.enable = true;
+  networking.hostName = "antanix"; # Define your hostname.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "latarcyrheb-sun32";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  system.autoUpgrade.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     pciutils htop
+     # utils:
+     p7zip keepassxc 
+     # dev tools
+     git jq tmux neovim
+     # web
+     wget curl firefoxWrapper google-chrome
+     # fonts
+     source-code-pro fontconfig-ultimate
+      # scala
+     sbt ammonite                                          
+     # window manager
+     i3lock i3status xorg.xbacklight networkmanagerapplet  
+     # productivity
+     slack zoom-us google-drive-ocamlfuse
+     # shell
+     zsh rxvt_unicode_with-plugins urxvt_font_size urxvt_theme_switch
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    keyMode = "vi";
+    terminal = "xterm-256color";
+    shortcut = "a";
+    clock24 = true;
+  };
+
+
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = [
+    pkgs.vaapiIntel
+    pkgs.libvdpau-va-gl
+    pkgs.vaapiVdpau
+    pkgs.intel-ocl
+    pkgs.linuxPackages.nvidia_x11.out
+  ];
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["modesetting"];
+
+    libinput = {
+      enable = true;
+      naturalScrolling = true;
+    };
+
+    xkbOptions = "eurosign:e";
+
+    layout = "gb";
+    monitorSection = ''
+      DisplaySize 406 228
+    '';
+
+    desktopManager = {
+      xterm.enable = false;
+      default = "none";
+      xfce = {
+        enable = false;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+
+    windowManager.i3 = {
+      enable = true;
+    };
+
+
+    displayManager.lightdm.enable = true;
+    # TODO: move to home
+    displayManager.sessionCommands = ''
+       xrdb "${pkgs.writeText  "xrdb.conf" ''
+          URxvt.font:                 xft:Source code pro:size=14
+          XTerm*faceName:             xft:Source code pro:size=14
+
+          !-- 
+	        URxvt.perl-ext-common: ...,font-size
+          URxvt.keysym.C-minus: perl:font-size:decrease
+          URxvt.keysym.C-equal: perl:font-size:increase
+          URxvt.font-size.step: 4
+
+          !-- the arc guy theme
+          urxvt*scrollBar: false
+          urxvt*matcher.button: 1
+          urxvt.transparent: false
+          urxvt.boldFont:
+          !-- Xft*dpi: 96
+          Xft*antialias: true
+          Xft*hinting: true
+          Xft*hintstyle: hintfull
+          URxvt*cursorUnderline:  true
+          Xft*rgba: rgb
+          URxvt*geometry: 85x20
+          *internalBorder: 23
+          URxvt*fading: 0
+          URxvt*tintColor: #ffffff
+          URxvt*shading: 0
+          URxvt*inheritPixmap: False
+          URxvt.color0: #3B444F
+          URxvt.color8: #4A585A
+          URxvt.color1: #CD546C
+          URxvt.color9: #CD546C
+          URxvt.color2: #A5AF86
+          URxvt.color10: #BDC79E
+          URxvt.color3: #CD7860
+          URxvt.color11: #E88773
+          URxvt.color4: #7A9388
+          URxvt.color12: #94B1A3
+          URxvt.color5: #75434C
+          URxvt.color13: #BB6C73
+          URxvt.color6: #767D75
+          URxvt.color14: #9DA69E
+          URxvt.color7: #C2C2A8
+          URxvt.color15: #DBDAC1
+          URxvt.background: #292929
+          URxvt.foreground: #a3a6ab
+       ''}"
+    '';
+
+
+    dpi = 168;
+  };
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [ source-code-pro ];
+    fontconfig.dpi = 168;
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.andrea = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = ["wheel" "docker" "networkmanager"];
+    shell = pkgs.zsh;
+  };
+
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
+
+}
--- vim.nix
+++ vim.nix
@@ -1,1 +1,100 @@
-../../../../etc/nixos/vim.nix
+with import <nixpkgs> {};
+
+pkgs.neovim.override {
+  vimAlias = true;
+  configure = {
+    customRC = ''
+      syntax enable
+
+      """ My settings:
+      " tab with two spaces
+      set nobackup
+      set noswapfile
+      set tabstop=2
+      set shiftwidth=2
+      set softtabstop=2
+      set expandtab
+      set smartcase
+      set autoindent
+      set nostartofline
+      set hlsearch      " highlight search terms
+      set incsearch     " show search matches as you type
+
+      set mouse=a
+      set cmdheight=2
+
+      set wildmenu
+      set showcmd
+
+      set number
+      set ruler
+      set backspace=indent,eol,start " Allows backspace on these character
+      set clipboard=unnamedplus
+
+      " Folding
+      set foldmethod=syntax
+
+      """ Keep all folds open when a file is opened
+      augroup OpenAllFoldsOnFileOpen
+          autocmd!
+          autocmd BufRead * normal zR
+      augroup END
+
+      """ Don't open folds with block motions
+      set foldopen-=block
+
+      " Map esc to exit terminal mode
+      :tnoremap <Esc> <C-\><C-n>
+
+
+
+      filetype plugin on    " Enable filetype-specific plugins
+
+      " vim-colors-solarized
+
+
+      " airline settings
+
+      set background=dark
+      colorscheme solarized
+
+      let g:airline_theme = 'solarized'
+      let g:airline_solarized_bg='dark'
+      let g:airline_powerline_fonts = 1
+      let g:airline#extensions#tabline#enabled = 1
+
+      " gitgutter settings
+      let g:gitgutter_max_signs = 2000
+
+      " vim-multiple-cursors settings
+      nnoremap <silent> <M-j> :MultipleCursorsFind <C-R>/<CR>
+      vnoremap <silent> <M-j> :MultipleCursorsFind <C-R>/<CR>
+
+      " ctrl-p settings
+      let g:ctrlp_max_files = 100000
+      let g:ctrlp_max_depth = 40
+
+      " Clean up artifacts in neovim, see https://github.com/neovim/neovim/issues/5990
+      let $VTE_VERSION="100"
+    '';
+
+    vam.knownPlugins = pkgs.vimPlugins; # optional
+    vam.pluginDictionaries = [
+      {
+        names = [
+          "airline"
+          "alchemist-vim"
+          "ctrlp"
+          "fugitive"
+          "surround"
+          "Solarized"
+          "multiple-cursors"
+          "syntastic"
+          "gitgutter"
+          "vim-airline-themes"
+          "vim-nix"
+        ];
+      }
+    ];
+  };
+}
