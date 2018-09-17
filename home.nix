550846bf5e9d93b793add72d8d2425c98ee1aa59
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2018-09-17 08:41:18 +0100

    keep track of nixos config

---
+++ home.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ 
+    pkgs.git
+    pkgs.zsh 
+    pkgs.zsh-powerlevel9k
+    pkgs.nix-zsh-completions
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    history = {
+      ignoreDups = true;
+      share = true;
+      save = 50000;
+    };
+    sessionVariables = {
+      "TERM" = "xterm-256color";
+      "EDITOR" = "vim";
+    };
+    initExtra = "source ${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme";
+  };
+  
+  programs.git = {
+    enable = true;
+    userName = "Andrea Fiore";
+    userEmail = "andrea.giulio.fiore@gmail.com";
+    aliases = {
+      "st" = "status";
+      "co" = "checkout";
+      "br" = "branch";
+    };
+  #  signing = {
+  #    key = "me@yrashk.com";
+  #    signByDefault = true;
+  #  };
+  };
+
+  programs.home-manager = {
+    enable = true;
+    path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+  };
+
+  manual.manpages.enable = true;
+
+  #xsession = {
+  #  enable = true;
+  #  pointerCursor = {
+  #    name = "Vanilla-DMZ";
+  #    size = 64;
+  #  };
+  #};
+}
+
